85616736e90c394c70c6747be0404abf78c7d7c0
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-02 19:59:14 +0200

    create

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
-using namespace std;
+#include "ArrayTemplate.h"
 
 int main() {
+    srand(time(0));
+
+    int A[6] = {6, 7, -3, 5, 2, -5};
+    prinArray(A, 6);
+
+    short B[10] = {0};
+    ranArray(B, 10);
+    prinArray(B, 10);
+
+    float D[15] = {0};
+    ranArray(D, 15, -100, 100);
+    prinArray(D, 15);
 
 
     return 0;
