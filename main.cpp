a181b9669f2ad8524ef88ed8ededced0ed7927da
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-02 20:43:54 +0200

    create

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include "ArrayTemplate.h"
 
+
+void calc_time(int hours, int minutes);
+void calc_time(int minutes);
+
 int main() {
     srand(time(0));
-
-    int A[6] = {6, 7, -3, 5, 2, -5};
-    prinArray(A, 6);
-
-    short B[10] = {0};
-    ranArray(B, 10);
-    prinArray(B, 10);
-
-    float D[15] = {0};
-    ranArray(D, 15, -100, 100);
-    prinArray(D, 15);
-
+    calc_time(567);
+    calc_time(1, 20);
 
     return 0;
+}
+
+void calc_time(int hours, int minutes) { //Перевантажені функції
+    cout << "Time: " << hours*60 + minutes << endl;
+}
+void calc_time(int minutes) {
+    cout << "Time: " << minutes/60  << ":" <<  minutes %60 << endl;
 }
